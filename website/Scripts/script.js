d9efb8e73230923166c1e38c1a32e54ecce71aab
chelseacx <chelsea.chen@my.jcu.edu.au>
2016-01-18 04:23:59 +0800

    Created an Order Online form page

--- website/Scripts/script.js
+++ website/Scripts/script.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 
 
+
 // The sub-menu is hidden initially
 $(document).ready(
 
@@ -39,6 +40,47 @@ $(document).ready(
 );
 
 
+
+//The following is for the product selection section of the online order form
+
+$(document).ready( function(){
+
+	$(".product_selection").hide().clone(true).appendTo("#product_selection_container").show();
+
+});
+
+$(document).ready( function(){
+
+	$(".remove_product").hide();
+
+});
+
+
+$(document).ready( function(){
+
+	$( ".add_product" ).click( function(){ 
+
+		$(".product_selection:first").clone(true).appendTo("#product_selection_container").slideDown();
+		
+		$(".product_selection:last .remove_product").show();
+	
+	});
+
+});
+
+$(document).ready( function(){
+
+	$( ".remove_product" ).click( function(){ 
+
+		$(this).parent().slideUp();
+	
+	});
+
+});
+
+
+
+
 //The following is for the slideshow on the index page
 
 
@@ -58,3 +100,24 @@ $(document).ready(
 
 
 
+
+
+
+$(document).ready( function(){
+
+	$( "#product_selection_container > p" ).click( function(){ 
+	
+		alert("cats are great");
+	
+	});
+
+});
+
+
+
+$(document).ready(function() {
+	
+
+	alert("FDSF");
+	
+});
