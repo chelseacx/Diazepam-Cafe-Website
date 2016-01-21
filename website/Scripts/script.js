71a64958522b1bb959b5ba4498f82d6f4a1be631
chelseacx <chelsea.chen@my.jcu.edu.au>
2016-01-21 16:16:57 +0800

    Added shopping cart to order form. Fixed the nav bar.

--- website/Scripts/script.js
+++ website/Scripts/script.js
@@ -20,8 +20,8 @@ $(document).ready(
 		
 		$("#nav_bar > ul > li").hoverIntent({
 			
-			over: function(){$(this).children("ul").slideDown(200)},
-			out: function(){$(this).children("ul").slideUp(200)},
+			over: function(){$(this).children("ul").slideDown(60)},
+			out: function(){$(this).children("ul").slideUp(100)},
 			timeout: 500
 		
 		})
@@ -41,106 +41,139 @@ $(document).ready(
 
 
 
-//The following is for the product selection section of the online order form
+//The following is for adding or removing products on the online order form
+
 
 $(document).ready( function(){
 
-	$(".product_selection").hide().clone(true).appendTo("#product_selection_container").show();
-	$(".remove_product").hide();
 
+	function updatePrice() {
 
-});
-
-
-
-//This adds and removes the product selection line
-
-$(document).ready( function(){
-
-	$( ".add_product" ).click( function(){ 
-
-		$(".product_selection:first").clone(true).appendTo("#product_selection_container").slideDown();
 		
-		$(".product_selection:last .remove_product").show();
+		var productCost; 
+		var productQuantity;
+		var productTotal = 0;
+		var calcTotal;
+		
+		$('span.product_cost:not(:first)').each( function(){
+			
+				productCost = $(this).text();
+				productQuantity = $(this).nextAll('span.product_quantity').text();
+				calcTotal = parseFloat(productCost) * parseInt(productQuantity);
+				$(this).nextAll('span.product_total').html(calcTotal);
+				
+				productTotal = productTotal + parseFloat(calcTotal);
+
+		});
+
+		$('#total_cost').html(productTotal);
+	
+	}
+
+
+	function updateHeight() {
+		
+	var formHeight = $('#order_form').height();
+	formHeight = formHeight + 400;
+	$('#order_form_container').css({
+		'min-height': formHeight,
+		'height': formHeight
+	});	
+		
+		
+	}
+
+
+
+	$('.product_selection_0').hide().clone(true).attr('class', "product_selection_1").appendTo('#product_selection_container').show();
+	
+	$('.remove_product').hide();
+
+
+	$('p.add_product').click( function(){
+	
+		var divClassName = $('#product_selection_container div:last-child').attr('class');
+		var classNumber = divClassName.substr(18, 1);
+
+		
+		classNumber = parseInt(classNumber) + 1;
+		divClassName = 'product_selection_' + classNumber;
+		
+		
+		$('.product_selection_0').clone(true).attr('class', divClassName).appendTo('#product_selection_container').slideDown();
+		
+		$('#product_selection_container div:last-child .remove_product').show();
+		
+		updatePrice();
+		updateHeight();
+	
+	});
+
+
+	$('.remove_product').click( function(){
+	
+		$(this).parent().remove();
+		
+		updatePrice();
+		updateHeight();
 	
 	});
 	
 	
-	$( ".remove_product" ).click( function(){ 
+	$('select.product_type').change( function() {
 
-		$(this).parent().slideUp();
+		var productType = $(this).val();
+		var productCost;
+		
+		
+		if (productType == "Single Scoop") 
+			{productCost = 3.5;}
+		else if (productType == "Double Scoop") 
+			{productCost = 5;} 
+		else if (productType == "Ice-Cream Cake") 
+			{productCost = 11;}
+		else if (productType == "Waffle Ice-Cream") 
+			{productCost = 4;}  
+		else if (productType == "Ice-Cream Cookies") 
+			{productCost = 3;} 
+		else if (productType == "Fried Ice-Cream") 
+			{productCost = 4;}
+		else 
+			{productCost = 0;}  
+		
+		
+		$(this).nextAll('.product_cost').html(productCost);
 	
 	});
 
+
+	$('input.quantity').change( function() {	
+		
+		var productQuantity = $(this).val();
+		$(this).nextAll('.product_quantity').html(productQuantity);
+
+	});
+
+	$('#product_selection_container').change(updatePrice);
+
 });
 
-
-
-// This is for calculating the total order cost
-
-/*
 
 $(document).ready( function(){
-	
+		
+	var formHeight = $('#order_form').height();
+	$('#order_form_container').atr('min-height', formHeight);
 
-	var productType = $(".product_type").val;
-	var productPrice; 
-	
-	if (productType = "single_scoop") {
-		
-		productPrice = 3.5;	
-	}
-
-	else if (productType = "double_scoop") {
-	
-		productPrice = 5;
-		
-	}
-	
-	else if (productType = "ice_cream_cake") {
-	
-		productPrice = 11;
-		
-	}	
-
-	else if (productType = "waffle_ice_cream") {
-	
-		productPrice = 4;
-		
-	}
-
-	else if (productType = "ice_cream_cookies") {
-	
-		productPrice = 3;
-		
-	}
-	
-	else if (productType = "fried_ice_cream") {
-	
-		productPrice = 4;
-		
-	}
-
-	else {
-	
-		productPrice = 0;
-		
-	}
-	
-	$('<p>').html(productPrice).insertAfter('.quantity');
 
 });
-
-*/
 
 
 
 //The following is for the slideshow on the index page
 
 
-$(document).ready(
-
-	function(){	
+$(document).ready( function(){
+		
 	$('.cycle-slideshow').cycle({
 		fx: 'fade',
 		speed: '8000',
@@ -148,9 +181,7 @@ $(document).ready(
 		easing: 'linear'
 		
 	});
-	}
-
-);
-
+	
+});
 
 
